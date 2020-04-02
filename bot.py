10bb22ed7b5e9b13a21e987770a6a2f74659cf79
Silvia <sarnaizgarcia@gmail.com>
2020-04-02 19:25:46 +0200

    Credentials and bot

--- bot.py
+++ bot.py
@@ -1,18 +1,18 @@
+import pendulum
+
 import telebot
+
 from telebot import types
+from repository.images_repo import images_repo
 
 bot = telebot.TeleBot('978325959:AAEbFC7LndG8iK8lwXwot0WiiSoJW_4sI0s')
 
 # https://github.com/eternnoir/pyTelegramBotAPI/blob/master/examples/step_example.py
 
-
-class User:
-    def __init__(self):
-        self.id = id
-        self.answer = None
-
-
-user = User()
+LON = -120.70418
+LAT = 38.32974
+BEGIN = '2000-01-01, END'
+END = pendulum.now('UTC').date().isoformat()
 
 
 @bot.message_handler(commands=['start'])
@@ -21,17 +21,27 @@ def welcome(message):
     # markup.add('Yes', 'No')
     msg = bot.reply_to(
         message, 'Hi! Would you like to help me to find wildfires?')  # , reply_markup=markup)
-    print('-----------------------------1', msg)
-
-    bot.register_next_step_handler(msg, get_coords)
+    bot.register_next_step_handler(msg, find_fire)
 
 
-def get_coords(message):
-    if (message.text == 'Yes'):
-        msg = bot.reply_to(message, 'Great!!')
-    else:
-        msg = bot.reply_to(message, 'Buhhhhh!!')
-        print('-----------------------------2', msg)
+@bot.message_handler(commands=['Yes', 'yes', 'Y', 'y'])
+def find_fire(message):
+    msg = bot.reply_to(message, 'Great!!')
+    images = images_repo.create_images(LON, LAT, BEGIN, END)
+    image = images_repo.request_image()
+    msg = bot.reply_to(message, 'Do you see the fire?')
+
+
+@bot.message_handler(commands=['Yes', 'yes', 'Y', 'y'])
+def afirmative_answer(message):
+    msg = images_repo.question_request('Y')
+
+
+@bot.message_handler(commands=['No', 'no', 'N', 'n'])
+def negative_answer(message):
+    msg = images_repo.question_request('N')
+    images = images_repo.create_images(LON, LAT, BEGIN, END)
+    image = images_repo.request_image()
 
 
 bot.polling()
